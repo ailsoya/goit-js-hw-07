6651b835f60f2059dfe445f63b94dd655857af48
ailsoya <akalitata53@gmail.com>
2023-08-03 15:23:32 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 
-const galler = document.querySelector(".gallery");
+const galler = document.querySelector(".gallery")
 
-console.log(galleryItems);
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+     </a>
+     </li>`).join("")
+}
+
+function getOriginal(event) {
+    event.preventDefault()
+    const imgOrig = event.target.dataset.source
+    const instance = basicLightbox.create(`<img src="${imgOrig}">`)
+    instance.show()
+}
+
+galler.insertAdjacentHTML('beforeend', createMarkup(galleryItems))
+galler.addEventListener('click', getOriginal);
